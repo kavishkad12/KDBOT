a816539eeb6a010b6adeb174151971efa6f884ab
KD STUDIO <kd1076347@gmail.com>
2025-01-20 22:03:16 +0530

    Update config.js
    Signed-off-by: KD STUDIO <kd1076347@gmail.com>

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "mZYBDJxB#S-KlJT7vYOHvYSYoP6nSnsoHpVT2mfOeIWaB7ALTB98"
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~w2lWiLrL#F958VUBlKIVv-9Djf_xulePuJom52Re4rMMD2omEY6g"
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/106251140?v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am KD STUDIO i am alive now|",    
 };
